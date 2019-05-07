5ce734877d8b903e1095b6e527fd6820a5023aac
Leandro Costa <leandro.costa@wolox.com.ar>
2019-05-07 16:53:36 -0300

    Merge with feature-controller-book + change BookRepository extends to CrudRepository

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,9 +1,9 @@
 package wolox.training.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import wolox.training.model.Book;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends CrudRepository<Book, Long> {
 
     Book findByAuthorIgnoreCase(String author);
 
