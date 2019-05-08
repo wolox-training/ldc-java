05ae29a8a33cb614f90962041ba07db367d6ac0a
Leandro Costa <leandro.costa@wolox.com.ar>
2019-05-08 15:55:13 -0300

    Se agrega annotation Repository en los repositorios

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,8 +1,10 @@
 package wolox.training.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import wolox.training.model.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
     User findFirstByUsername(String userName);
