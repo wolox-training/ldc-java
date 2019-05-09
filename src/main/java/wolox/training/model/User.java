32696d75c2926961146dd8ab95b4d75d9e87b678
Leandro Costa <leandro.costa@wolox.com.ar>
2019-05-09 10:53:02 -0300

    I deleted the setId method because the id parameter should be generate automatically

--- src/main/java/wolox/training/model/User.java
+++ src/main/java/wolox/training/model/User.java
@@ -49,10 +49,6 @@ public class User {
         return id;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public String getUsername() {
         return username;
     }
