64c5d358408bcb67507e3750fc5eb60a5b7e4900
Leandro Costa <leandro.costa@wolox.com.ar>
2019-05-14 11:58:15 -0300

    I added an endpoint to get the username

--- src/main/java/wolox/training/controller/UserController.java
+++ src/main/java/wolox/training/controller/UserController.java
@@ -1,5 +1,6 @@
 package wolox.training.controller;
 
+import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -75,6 +77,12 @@ public class UserController {
             orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                 "User with id " + id + " not found"));
         return userRepository.save(user);
+    }
+
+    @GetMapping("/username")
+    @ResponseBody
+    public String currentUserName(Principal principal) {
+        return principal.getName();
     }
 
 }
